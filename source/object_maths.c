bdc0c4ffaad523bc3dfe7b41a08e09afb1f297a9
batburger <a_modenbach@outlook.com>
2018-05-26 18:31:13 +0200

    Updated for changed object_t interface

--- source/object_maths.c
+++ source/object_maths.c
@@ -22,30 +22,15 @@ bool larger_then(object_t a, object_t b) {
 
 object_t add(object_t a, object_t b){
     object_t out;
-    double result;
-    double a_value = a.value;
-    double b_value = b.value;
-
-    if(a.type == OBJECT_FLOAT)
-        a_value = a.double_value;
-
-    if(b.type == OBJECT_FLOAT)
-        b_value = b.double_value;
 
     if(a.signage == b.signage){
-        result = a_value + b_value;
+        out.value = a.value + a.value;
         out.signage = a.signage;
     }else{
-        result = underflow_save_subtract(a_value, b_value, &out.signage);
+        out.value = underflow_save_subtract(a.value, b.value, &out.signage);
     }
 
-    out.type = OBJECT_NUMBER;
-    out.value = (bitarr_t)result;
-    if(a.type == OBJECT_FLOAT || b.type == OBJECT_FLOAT){
-        out.type = OBJECT_FLOAT;
-        out.double_value = result;
-    }
-
+    out.type = (a.type == OBJECT_FLOAT || b.type == OBJECT_FLOAT) ? (OBJECT_FLOAT) : (OBJECT_NUMBER);
     out.size = (a.size > b.size) ? (a.size) : (b.size);
 
     return out;
@@ -53,62 +38,32 @@ object_t add(object_t a, object_t b){
 
 object_t subtract(object_t a, object_t b){
     object_t out;
-    double result;
-    double a_value = a.value;
-    double b_value = b.value;
-
-    out.type = OBJECT_NUMBER;
-    out.size = (a.size > b.size) ? (a.size) : (b.size);
-
-    if (a.type == OBJECT_FLOAT){
-        a_value = a.double_value;
-        out.type = OBJECT_FLOAT;
-    }
-    if (b.type == OBJECT_FLOAT){
-        b_value = b.double_value;
-        out.type = OBJECT_FLOAT;
-    }
 
     if(a.signage == b.signage){
         if(a.signage == 1){
             b.signage = 0;
-            return add(a, b);
+            out = add(a, b);
         }else{
-            result = underflow_save_subtract(a_value, b_value, &out.signage);
+            out.value = underflow_save_subtract(a.value, b.value, &out.signage);
         }
     }else{
-        result = a_value + b_value;
+        out.value = a.value + b.value;
         out.signage = (a.signage == 1) ? (1) : (0);
     }
 
-    out.value = (bitarr_t)result;
-    if(a.type == OBJECT_FLOAT || b.type == OBJECT_FLOAT)
-        out.double_value = result;
+    out.type = (a.type == OBJECT_FLOAT || b.type == OBJECT_FLOAT) ? (OBJECT_FLOAT) : (OBJECT_NUMBER);
+    out.size = (a.size > b.size) ? (a.size) : (b.size);
 
     return out;
 }
 
 object_t multiply(object_t a, object_t b){
     object_t out;
-    double a_value = a.value;
-    double b_value = b.value;
-    double result;
 
-    if(a.type == OBJECT_FLOAT)
-        a_value = a.double_value;
-    if(a.type == OBJECT_FLOAT)
-        b_value = b.double_value;
-
-    result = a_value * b_value;
+    out.value = a.value * b.value;
     out.size = (a.size > b.size) ? (a.size) : (b.size);
     out.signage = (a.signage == b.signage) ? (0) : (1);
-
-    out.type = OBJECT_NUMBER;
-    out.value = (bitarr_t)result;
-    if(a.type == OBJECT_FLOAT || b.type == OBJECT_FLOAT){
-        out.double_value = result;
-        out.type = OBJECT_FLOAT;
-    }
+    out.type = (a.type == OBJECT_FLOAT || b.type == OBJECT_FLOAT) ? (OBJECT_FLOAT) : (OBJECT_NUMBER);
 
     return out;
 }
@@ -116,21 +71,9 @@ object_t multiply(object_t a, object_t b){
 object_t divide(object_t a, object_t b){
     object_t out;
 
-    double a_value = a.value;
-    double b_value = b.value;
-
-    if(a.type == OBJECT_FLOAT)
-        a_value = a.double_value;
-    if(a.type == OBJECT_FLOAT)
-        b_value = b.double_value;
-
-    out.double_value = a_value / b_value;
-    out.type = OBJECT_FLOAT;
-
-    if((bitarr_t)out.double_value == out.double_value)
-        out.type = OBJECT_NUMBER;
-
-    out.size = sizeof(uint32_t);
+    out.value = a.value / b.value;
+    out.type = (a.type == OBJECT_FLOAT || b.type == OBJECT_FLOAT) ? (OBJECT_FLOAT) : (OBJECT_NUMBER);
+    out.size = sizeof(double);
     out.signage = (a.signage != b.signage) ? (1) : (0);
     return out;
 }
